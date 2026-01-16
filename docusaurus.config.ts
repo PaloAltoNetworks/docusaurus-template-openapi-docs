69c1d07d56795633772a2e245ce78ce90ef5e0e3
Steven Serrata <sserrata@paloaltonetworks.com>
2026-01-16 12:23:29 -0600

    upgrade to 4.6.0

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,8 +12,13 @@ const config: Config = {
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
